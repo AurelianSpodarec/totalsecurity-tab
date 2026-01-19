b1635a37ce1f2b05a85590fb71c2110f1387a2f4
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2026-01-19 13:51:48 +0000

    chore: remove irrelevant comments

--- packages/ext-api/index.ts
+++ packages/ext-api/index.ts
@@ -1,16 +1,12 @@
-// Runtime API
 export { RuntimeApi } from "./src/Api/Runtime/RuntimeApi";
 export type { MessageSender, OnInstalledReason } from "./src/Api/Runtime/types";
 
-// SidePanel API
 export { SidePanelApi } from "./src/Api/SidePanel/SidePanelApi";
 export type { PanelBehaviour } from "./src/Api/SidePanel/types";
 
-// Storage API
 export { LocalStorageApi } from "./src/Api/Storage/LocalStorageApi";
 export type { StorageChange } from "./src/Api/Storage/types";
 
-// TabGroups API
 export { TabGroupsApi } from "./src/Api/TabGroups/TabGroupsApi";
 export type {
   TabGroup,
@@ -20,7 +16,6 @@ export type {
   TabGroupsUpdateInfo,
 } from "./src/Api/TabGroups/types";
 
-// Tabs API
 export { TabsApi } from "./src/Api/Tabs/TabsApi";
 export type {
   Tab,
@@ -31,7 +26,6 @@ export type {
   TabCreateProperties,
 } from "./src/Api/Tabs/types";
 
-// Windows API
 export { WindowsApi } from "./src/Api/Windows/WindowsApi";
 export type {
   Window,
